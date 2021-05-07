07abea35fefcd08c88adaf914b53fcc7688f4a88
Yash-Singh1 <saiansh2525@gmail.com>
2021-05-07 11:16:22 -0700

    1.1.2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var ide = {};
 
 ide.name = 'monkeyide';
-ide.version = '1.1.1';
+ide.version = '1.1.2';
 ide.license = 'MIT';
 ide.buttonsBar = document.createElement('div');
 ide.buttonsBar.className = 'tabbuttons';
